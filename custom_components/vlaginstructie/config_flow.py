13011df5ab87f509b38005e28b54fc1cf312d9a2
Marten Jansen <marten@martenjansen.nl>
2025-09-17 16:10:39 +0200

    fix config flow

--- custom_components/vlaginstructie/config_flow.py
+++ custom_components/vlaginstructie/config_flow.py
@@ -1,8 +1,8 @@
 import voluptuous as vol
 from homeassistant import config_entries
-from homeassistant.core import callback
 
-from . import DOMAIN
+from .const import DOMAIN  # zorg dat DOMAIN in const.py staat: DOMAIN = "vlaginstructie"
+
 
 class VlaginstructieConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for Vlaginstructie."""
@@ -12,13 +12,14 @@ class VlaginstructieConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     async def async_step_user(self, user_input=None):
         """First step of the flow (called from UI)."""
         if user_input is not None:
-            return self.async_create_entry(title="Vlaginstructie Nederland", data={})
+            return self.async_create_entry(
+                title="Vlaginstructie Nederland", data={}
+            )
 
         return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
 
-    @staticmethod
-    @callback
-    def async_get_options_flow(config_entry):
+    async def async_get_options_flow(self, config_entry):
+        """Return the options flow handler."""
         return VlaginstructieOptionsFlowHandler(config_entry)
 
 
@@ -29,6 +30,7 @@ class VlaginstructieOptionsFlowHandler(config_entries.OptionsFlow):
         self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
+        """Manage the options."""
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
