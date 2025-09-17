f0096173f96451dad983533350c85038758cc179
Marten Jansen <31031670+tjmjansen@users.noreply.github.com>
2025-09-17 16:33:12 +0200

    fix config flow (#13)

--- custom_components/vlaginstructie/__init__.py
+++ custom_components/vlaginstructie/__init__.py
@@ -1,2 +1,16 @@
-# Minimal __init__.py
+"""VlagInstructie integration."""
+
+from homeassistant.core import HomeAssistant
+from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
+from . import sensor
+
+async def async_setup(hass: HomeAssistant, config: dict):
+    """Set up is not used because this integration uses config flow."""
+    return True
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
+    """Set up VlagInstructie from a config entry."""
+    # registreer de sensors
+    await sensor.async_setup_entry(hass, entry, hass.async_add_entities)
+    return True
