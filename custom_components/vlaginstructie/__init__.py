5bafc373abd06ef0e25722418e64fd24bbb27348
Marten Jansen <marten@martenjansen.nl>
2025-09-17 16:24:52 +0200

    fix config flow part 2

--- custom_components/vlaginstructie/__init__.py
+++ custom_components/vlaginstructie/__init__.py
@@ -1,23 +1,2 @@
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import HomeAssistant
-from . import DOMAIN
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up via configuration.yaml (legacy)."""
-    return True
-
-async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Set up via UI config entries."""
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "sensor")
-    )
-    hass.async_create_task(
-        hass.config_entries.async_forward_entry_setup(entry, "binary_sensor")
-    )
-    return True
-
-async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
-    """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, "sensor")
-    unload_ok &= await hass.config_entries.async_forward_entry_unload(entry, "binary_sensor")
-    return unload_ok
+# Minimal __init__.py
+from .const import DOMAIN
